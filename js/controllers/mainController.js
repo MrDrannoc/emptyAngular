b18cd6d45b7b37162237c47577a2c53b17d08f88
MrDrannoc <poyer.k@gmail.com>
2017-01-09 14:18:46 +0100

    first commit

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,4 +1,23 @@
 // MAIN CONTROLLER
-function mainController($scope) {
-    
+function mainController() {
+  
+    this.fiches = [];
+
+    this.create = (nom, prenom, email) => {
+        this.fiches.push({nom: nom, prenom: prenom, email: email});
+        this.nom = "";
+        this.prenom = "";
+        this.email = "";
+        console.log(this.fiches);
+    };
+
+    this.update = (i, nom, prenom, email) => {
+        this.fiches[i] = {nom: nom, prenom: prenom, email: email};
+        console.log(this.fiches);
+    };
+
+    this.delete = (i) => {
+        this.fiches.splice(i, 1);
+        console.log(this.fiches);
+    };
 }
